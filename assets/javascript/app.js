12a7cca6b811e5be08dd8e3825a2068da9fa54cf
David <djlowes@gmail.com>
2017-10-18 21:52:38 -0700

    added restart game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -109,10 +109,12 @@ var intervalId;
 var winCount = 0;
 var loseCount = 0;
 var unanswerCount = 0;
+$("#playagain").hide();
 
 
-$("#footer").on("click", function() {
-		$("#footer").hide();
+$("#start").on("click", function() {
+		$("#start").hide();
+		$("#playagain").show();
 		$("#left").show();
 		$("#right").show();
 		showQuestions();
@@ -121,30 +123,42 @@ $("#footer").on("click", function() {
 
 function showQuestions() {
 		runClock();
-		$("#timer").show();
-		$("#result").hide();
 		$("#question").html(triviaQuestions[questionCount].question);
 		$("#q1").html(triviaQuestions[questionCount].answers.a);
 		$("#q2").html(triviaQuestions[questionCount].answers.b);
 		$("#q3").html(triviaQuestions[questionCount].answers.c);
 		$("#q4").html(triviaQuestions[questionCount].answers.d);
+			if (questionCount ===9) {
+				over();
+			}
 }
-
 
 $(".qs").on("click", function() {
 userGuess = $(this).attr("value");
 	if (userGuess === triviaQuestions[questionCount].correctAnswer) {
+		console.log("Correct Guess")
 		winCount++;
+		questionCount++
+		$("#timer").hide()
+		$("#result").show()
+		$("#result").html("You got it right! Well done!");
 		clearInterval(intervalId);
-		setTimeout(correct, 4000);
+		setTimeout(reset, 4000);
 	} else {
+		console.log("Incorrect Guess")
 		loseCount++;
+		questionCount++
+		$("#timer").hide()
+		$("#result").show()
+		$("#result").html("You didn't get it this time. Try again");
 		clearInterval(intervalId);
-		setTimeout(incorrect, 4000);
+		setTimeout(reset, 4000);
 	}
 });
 
 function runClock(){
+	$("#timer").show();
+	$("#result").hide()
 	intervalId = setInterval(decrement, 1000);
 }
 
@@ -152,41 +166,37 @@ function decrement() {
   countDown--;
   $("#timer").html(countDown);
     if (countDown === 0) {
-		unanswerCount++
-		$("#timer").hide()
-			$("#timerwrap").html("You are out of time!!");
-        	console.log("Out of time");
-			stop();
-      }
-    }
-
-function stop() {
-	running = false;
-	clearInterval(intervalId);
-	showQuestions()
+			unanswerCount++
+			questionCount++
+			$("#timer").hide()
+			$("#result").show()
+			$("#result").html("You are out of time!!");
+      console.log("Out of time");
+			clearInterval(intervalId);
+			setTimeout(reset, 4000);
+   }
 }
 
-
-var correct = function() {
-	//$(this).find('div').css("background-color", "yellow");
-	$("#timer").hide()
-	$("#result").show();
-	$("#result").html("Well done, you guessed correct!");
-	console.log("Correct Guess")
-	questionCount++
+var reset = function() {
 	countDown = 30
 	showQuestions();
 }
 
-var incorrect = function () {
-	$("#timer").hide()
-	$("#result").show();
-	$("#result").html("You Guessed Wrong");
-	console.log("Wrong Guess")
-	questionCount++
-	countDown = 30
-	showQuestions();
+function over() {
+	$("#left").hide();
+	$("#right").hide();
 }
+
+$("#playagain").on("click", function() {
+	var questionCount = 0
+	var countDown = 30;
+	var intervalId;
+	var winCount = 0;
+	var loseCount = 0;
+	var unanswerCount = 0;
+
+});
+
 
 
 });
